6c7d0e703ba1cf65ae3b72d4b5585bc3de1fbff5
kazutoichikawa <nekomaru33281124@yahoo.co.jp>
2020-12-01 17:06:55 +0900

    各モデルのテスト

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,8 +4,10 @@ class Message < ApplicationRecord
   has_one_attached :image
 
   validates :content, presence: true, unless: :was_attached?
+  #contentが空だとダメですよ、ただしファイルが添付されてますか？がtrueの場合は除く
 
-  def was_attached?
+  def was_attached? #ファイルは添付されてますか？
     self.image.attached?
   end
 end
+
